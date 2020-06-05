6d87fdd8d8bf5d901d00bda0acf8ec326de11dbc
NessMC <contact@nessmc.fr>
2020-06-05 12:13:09 +0200

    📚 Packages loaded in main file.

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
           DISCORD BOT TEMPLATE
                   Main
 //////////////////////////////////////*/
+
+import Discord from 'discord.js' // Importing Discord.js package
+import FS      from 'fs'         // Importing FS package
+import PATH    from 'path'       // Importing PATH package
