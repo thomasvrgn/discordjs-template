705d792555498dd2a538949ee7d90b1c876648c6
NessMC <contact@nessmc.fr>
2020-06-05 12:44:08 +0200

    🔧 Configuration file added.

---
+++ config.js
@@ -0,0 +1,43 @@
+/*//////////////////////////////////////
+          DISCORD BOT TEMPLATE
+             Configuration
+//////////////////////////////////////*/
+
+export default {
+
+    prefix          : '!'                         , // Bot command prefix
+    name            : 'YOUR BOT NAME HERE'        , // Bot loading title
+    description     : 'YOUR BOT DESCRIPTION HERE' , // Bot loading description
+    
+    color           : '#ff0000'                   , // Bot embed color
+ 
+    loading         : {                             // Loading informations
+
+        info        : {                             // Informations loading
+            color   : '#'                         ,
+            message : 'test'
+        }                                         , 
+        success     : {                             // Success loading
+            color   : '#'                         ,
+            message : 'test'
+        }                                         , 
+        error       : {                             // Error loading
+            color   : '#'                         ,
+            message : 'test'
+        }                                         , 
+
+        commands    : {                             // Commands loading
+            color   : '#'                         ,
+            message : 'test'
+        }                                         , 
+        events      : {                             // Events loading
+            color   : '#'                         ,
+            message : 'test'
+        }                                         , 
+        listeners   : {                             // Listeners loading
+            color   : '#'                         ,
+            message : 'test'
+        }
+
+    }
+}
